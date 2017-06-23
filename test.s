918e2360cfa4e8e17e82a23f9e15b9431fe40812
archdoor <archdoor@outlool.com>
2017-06-23 17:34:50 +0800

    files update

--- test.s
+++ test.s
@@ -1,15 +1,20 @@
 .section .data
 value1:
-	.byte 0x34, 0x12, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00
+	.float 12.34, 2345.543, -3493.2, 0.44901
 value2:
-	.int 2
+	.float -5439.234, 32121.4, 1.0094, 0.000003
+
+.section .bss
+	.lcomm data, 16
 
 .section .text
 .global _start
 _start:
-	fbld value1
-	fimul value2
-	fbstp value1
+	nop
+	movups value1, %xmm0
+	movups value2, %xmm1
+	movups %xmm0, %xmm2
+	movups %xmm0, data
 
 	movl $1, %eax
 	movl $0, %ebx
